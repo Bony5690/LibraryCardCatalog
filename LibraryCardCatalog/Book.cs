7fa02d6789a42aa6bb671b3195cc6fe35bb537bb
jerry bony <jerrybony@yahoo.com>
2016-11-14 10:57:24 -0600

    Add project files.

---
+++ LibraryCardCatalog/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace LibraryCardCatalog
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Subject { get; set; }
+
+        public string title;
+        public string author;
+        public string subject;
+    }
+}
---
+++ LibraryCardCatalog/CardCatalog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryCardCatalog
+{
+    [DataContract]
+    class CardCatalog
+    {
+        [DataMember]
+        public List<Book> BookListHolder = new List<Book>();
+
+        [DataMember]
+        public string filename;
+
+        //  private Book books;
+
+        public void CatalogMenu()
+        {
+
+        }
+
+    }
+}
